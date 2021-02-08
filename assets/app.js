3e31b0eff654aa469687b166713183dca7b8f912
Shahinoor Alam <73857004+shahinooriatc@users.noreply.github.com>
2021-02-08 22:15:25 +0600

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -41,8 +41,8 @@ const findFoodInfo = food => {
     <img class="img-fluid rounded mb-4" src="${food.strMealThumb}" alt="">
     <h4>${food.strMeal}</h4>
     
-    <h5 class="pt-3 pb-2"><i class="icon-fire icons"></i> Ingredients</h5>
-    <ul class="list-unstyled mb-0">${ingredients.map((ingredient) => `<li><i class="icon-check icons"></i>${ingredient}</li>`).join('')}</ul>
+    <h5 class="pt-3 pb-2"><i class="icon-fire icons text-danger"></i> Ingredients</h5>
+    <ul class="list-unstyled mb-0">${ingredients.map((ingredient) => `<li><i class="icon-check icons text-success my-2"></i>${ingredient}</li>`).join('')}</ul>
 `;
 };
 
